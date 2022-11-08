c27efa6ca6501997a6a59b14531dded723d745bf
min56 <minchaeg85@gmail.com>
2022-11-08 20:45:26 +0900

    📝::

--- src/main/java/com/example/memoir/domain/user/controller/UserController.java
+++ src/main/java/com/example/memoir/domain/user/controller/UserController.java
@@ -8,6 +8,7 @@ import com.example.memoir.domain.user.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
